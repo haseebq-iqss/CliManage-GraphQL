7e5eb8345714115c908e507ddf504d9227e23184
Haseeb Qureshi <haseeb.qureshi@iquasar.com>
2023-10-15 00:29:22 +0530

    Cors +++

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const schema = require("./schema/schema");
 const { graphqlHTTP } = require("express-graphql");
 const connectDb = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 connectDb();
 
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
